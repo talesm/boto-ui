77eb060663c79d3374202ec36359991d1baf91e2
talesm <tales.miranda84@gmail.com>
2020-11-21 17:39:31 -0300

    Change literals into constants

--- baseWidgets.hpp
+++ baseWidgets.hpp
@@ -6,11 +6,20 @@
 
 namespace dui {
 
+namespace style {
+
+constexpr SDL_Color TEXT{0, 0, 0, 255};
+constexpr SDL_Color BUTTON{224, 224, 224, 255};
+constexpr SDL_Color BUTTON_LIGHT{255, 255, 255, 255};
+constexpr SDL_Color BUTTON_DARK{0, 0, 0, 255};
+
+}
+
 inline void
 renderLabel(Frame& frame,
             std::string_view text,
             const SDL_Point& p,
-            SDL_Color color)
+            SDL_Color color = style::TEXT)
 {
   frame.string(p, color, text);
 }
@@ -19,7 +28,7 @@ inline void
 label(Frame& frame,
       std::string_view text,
       const SDL_Point& p,
-      SDL_Color color = {0, 0, 0, 255})
+      SDL_Color color = style::TEXT)
 {
   auto adv = frame.measure(text);
   adv.x += p.x + 2;
@@ -29,23 +38,27 @@ label(Frame& frame,
 }
 
 inline void
-renderButton(Frame& frame, const SDL_Rect& r)
+renderButton(Frame& frame,
+             const SDL_Rect& r,
+             SDL_Color b = style::BUTTON,
+             SDL_Color l = style::BUTTON_LIGHT,
+             SDL_Color d = style::BUTTON_DARK)
 {
-  frame.box({r.x + 1, r.y, r.w - 2, 1}, {255, 255, 255, 255});
-  frame.box({r.x, r.y + 1, 1, r.h - 2}, {255, 255, 255, 255});
-  frame.box({r.x + 1, r.y + r.h - 2 + 1, r.w - 2, 1}, {0, 0, 0, 224});
-  frame.box({r.x + r.w - 2 + 1, r.y + 1, 1, r.h - 2}, {0, 0, 0, 224});
-  frame.box({r.x + 1, r.y + 1, r.w - 2, r.h - 2}, {224, 224, 224, 255});
+  frame.box({r.x + 1, r.y, r.w - 2, 1}, {l.r, l.g, l.b, l.a});
+  frame.box({r.x, r.y + 1, 1, r.h - 2}, {l.r, l.g, l.b, l.a});
+  frame.box({r.x + 1, r.y + r.h - 2 + 1, r.w - 2, 1}, {d.r, d.g, d.b, d.a});
+  frame.box({r.x + r.w - 2 + 1, r.y + 1, 1, r.h - 2}, {d.r, d.g, d.b, d.a});
+  frame.box({r.x + 1, r.y + 1, r.w - 2, r.h - 2}, {b.r, b.g, b.b, b.a});
 }
 
 inline void
-renderButtonPressed(Frame& frame, const SDL_Rect& r)
+renderButtonPressed(Frame& frame,
+                    const SDL_Rect& r,
+                    SDL_Color b = style::BUTTON,
+                    SDL_Color l = style::BUTTON_LIGHT,
+                    SDL_Color d = style::BUTTON_DARK)
 {
-  frame.box({r.x + 1, r.y, r.w - 2, 1}, {0, 0, 0, 224});
-  frame.box({r.x, r.y + 1, 1, r.h - 2}, {0, 0, 0, 224});
-  frame.box({r.x + 1, r.y + r.h - 2 + 1, r.w - 2, 1}, {255, 255, 255, 255});
-  frame.box({r.x + r.w - 2 + 1, r.y + 1, 1, r.h - 2}, {255, 255, 255, 255});
-  frame.box({r.x + 1, r.y + 1, r.w - 2, r.h - 2}, {224, 224, 224, 255});
+  renderButton(frame, r, b, d, l);
 }
 
 inline bool
@@ -59,7 +72,7 @@ button(Frame& frame, std::string_view text, bool inverted, const SDL_Point& p)
   } else {
     renderButton(frame, r);
   }
-  renderLabel(frame, text, {p.x + 1, p.y + 1}, {0, 0, 0, 255});
+  renderLabel(frame, text, {p.x + 1, p.y + 1});
   adv.x += p.x + 2;
   adv.y += p.y + 2;
   frame.advance(adv);
