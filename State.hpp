e2c3c1dd0622b9f96fa33fbbc1f102702aa87fd6
talesm <tales.miranda84@gmail.com>
2020-11-26 20:09:45 -0300

    Allow multiple elements with same id in different groups

--- State.hpp
+++ State.hpp
@@ -6,6 +6,8 @@
 
 namespace dui {
 
+constexpr char groupNameSeparator = '/';
+
 enum class MouseAction
 {
   NONE,   ///< Default status
@@ -35,6 +37,10 @@ private:
   char tBuffer[SDL_TEXTINPUTEVENT_TEXT_SIZE];
   bool tChanged = false;
   TextAction tAction = TextAction::NONE;
+
+  std::string group;
+  bool gGrabbed = false;
+  bool gActive = false;
 
 public:
   State(SDL_Renderer* renderer)
@@ -79,14 +85,17 @@ public:
 
   bool isInFrame() const { return inFrame; }
 
-  bool isActive(std::string_view id) const { return eActive == id; }
+  bool isActive(std::string_view id) const
+  {
+    return isSameGroupId(eActive, id);
+  }
 
   /// Check mouse for given element
   MouseAction testMouse(std::string_view id, SDL_Rect r);
 
   TextAction checkText(std::string_view id) const
   {
-    if (!tChanged || eActive != id) {
+    if (!tChanged || !isSameGroupId(eActive, id)) {
       return TextAction::NONE;
     }
     return tAction;
@@ -128,9 +137,11 @@ public:
     void pushGroup(std::string_view id, const SDL_Rect&)
     {
       state->dList.popClip();
+      state->beginGroup(id);
     }
     void popGroup(std::string_view id, const SDL_Rect& r)
     {
+      state->endGroup(id);
       state->dList.pushClip(r);
     }
   };
@@ -151,34 +162,133 @@ private:
     inFrame = false;
     tChanged = false;
   }
+
+  void beginGroup(std::string_view id);
+  void endGroup(std::string_view id);
+  bool isSameGroupId(std::string_view qualifiedId, std::string_view id) const;
 };
+
+inline bool
+State::isSameGroupId(std::string_view qualifiedId, std::string_view id) const
+{
+  auto groupSize = group.size();
+  if (qualifiedId.size() < groupSize + 1) {
+    return false;
+  }
+  if (qualifiedId.substr(0, groupSize) != group ||
+      qualifiedId[groupSize] != groupNameSeparator ||
+      qualifiedId.substr(groupSize + 1) != id) {
+    return false;
+  }
+  return true;
+}
 
 inline MouseAction
 State::testMouse(std::string_view id, SDL_Rect r)
 {
   SDL_assert(inFrame);
   if (eGrabbed.empty()) {
-    if (mLeftPressed && SDL_PointInRect(&mPos, &r)) {
-      eGrabbed = id;
-      eActive = id;
+    if (!mLeftPressed) {
+      return MouseAction::NONE;
+    }
+    if (SDL_PointInRect(&mPos, &r)) {
+      eGrabbed = group;
+      eGrabbed += groupNameSeparator;
+      eGrabbed += id;
+      eActive = group;
+      eActive += groupNameSeparator;
+      eActive += id;
+      gGrabbed = true;
+      gActive = true;
       return MouseAction::GRAB;
     }
-    if (mLeftPressed && eActive == id) {
+    if (isSameGroupId(eActive, id)) {
+      gActive = false;
       eActive.clear();
     }
     return MouseAction::NONE;
   }
-  if (eGrabbed != id) {
+  if (!isSameGroupId(eGrabbed, id)) {
     return MouseAction::NONE;
   }
   if (mLeftPressed) {
     return SDL_PointInRect(&mPos, &r) ? MouseAction::GRAB : MouseAction::DRAG;
   }
   eGrabbed.clear();
+  gGrabbed = true;
   if (!SDL_PointInRect(&mPos, &r)) {
     return MouseAction::NONE;
   }
   return MouseAction::ACTION;
+}
+
+inline void
+State::beginGroup(std::string_view id)
+{
+  if (id.empty()) {
+    return;
+  }
+  auto idSize = id.size();
+  auto groupSize = group.size();
+  if (groupSize > 0) {
+    group += groupNameSeparator;
+    if (!gGrabbed || eGrabbed.size() <= idSize + groupSize + 1 ||
+        std::string_view{eGrabbed}.substr(groupSize, idSize + 1) != id ||
+        eGrabbed[groupSize + idSize + 1] != groupNameSeparator) {
+      gGrabbed = false;
+    }
+    if (!gActive || eActive.size() <= idSize + groupSize + 1 ||
+        std::string_view{eActive}.substr(groupSize, idSize + 1) != id ||
+        eActive[groupSize + idSize + 1] != groupNameSeparator) {
+      gActive = false;
+    }
+  } else {
+    if (eGrabbed.size() <= idSize ||
+        std::string_view{eGrabbed}.substr(0, idSize) != id ||
+        eGrabbed[idSize] != groupNameSeparator) {
+      gGrabbed = false;
+    } else {
+      gGrabbed = true;
+    }
+    if (eActive.size() <= idSize ||
+        std::string_view{eActive}.substr(0, idSize) != id ||
+        eActive[idSize] != groupNameSeparator) {
+      gActive = false;
+    } else {
+      gActive = true;
+    }
+  }
+  group += id;
+}
+
+inline void
+State::endGroup(std::string_view id)
+{
+  if (id.empty()) {
+    return;
+  }
+  if (id.size() >= group.size()) {
+    SDL_assert(group == id);
+    group.clear();
+    gActive = gGrabbed = false;
+  } else {
+    auto groupSize = group.size();
+    auto nextSize = groupSize - id.size() - 1;
+    SDL_assert(group[nextSize] == groupNameSeparator);
+    SDL_assert(std::string_view{group}.substr(nextSize + 1) == id);
+
+    group.erase(group.begin() + nextSize, group.end());
+    if (!gGrabbed && eGrabbed.size() > nextSize &&
+        std::string_view{eGrabbed}.substr(0, nextSize) == group &&
+        eGrabbed[nextSize] == groupNameSeparator) {
+      gGrabbed = true;
+    }
+    if (!gActive && eActive.size() > nextSize &&
+        std::string_view{eActive}.substr(0, nextSize) == group &&
+        eActive[nextSize] == groupNameSeparator) {
+      gActive = true;
+    }
+  }
 }
 } // namespace dui
 
