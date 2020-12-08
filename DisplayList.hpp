ba37f42b2932359c2ea8493f1a5af2a98c2ce192
talesm <tales.miranda84@gmail.com>
2020-12-08 14:54:22 -0300

    Remove cleverness for DisplayList

--- DisplayList.hpp
+++ DisplayList.hpp
@@ -47,8 +47,35 @@ struct Shape
  */
 class DisplayList
 {
-private:
-  std::vector<Shape> items;
+  enum CommandType
+  {
+    POP_CLIP,
+    PUSH_CLIP,
+    SHAPE,
+  };
+
+  struct Command
+  {
+    union
+    {
+      Shape shape;
+      SDL_Rect rect;
+    };
+    CommandType type;
+
+    Command()
+      : type(POP_CLIP)
+    {}
+    Command(const Shape& shape)
+      : shape(shape)
+      , type(SHAPE)
+    {}
+    Command(const SDL_Rect& rect)
+      : rect(rect)
+      , type(PUSH_CLIP)
+    {}
+  };
+  std::vector<Command> items;
 
 public:
   void clear() { items.clear(); }
@@ -66,13 +93,13 @@ public:
   {
     // TODO coalesce multiple clips
     if (rect.w > 0 && rect.h > 0) {
-      items.push_back(Shape{nullptr, rect, {0}, 0});
+      items.push_back(rect);
     } else {
-      items.push_back(Shape{nullptr, {rect.x, rect.y, 1, 1}, {0}, 0});
+      items.push_back(SDL_Rect{rect.x, rect.y, 1, 1});
     }
   }
 
-  void popClip() { items.push_back(Shape{nullptr, {0}, {0}, 0}); }
+  void popClip() { items.push_back({}); }
 
   void render(SDL_Renderer* renderer) const;
 };
@@ -90,27 +117,30 @@ DisplayList::render(SDL_Renderer* renderer) const
   SDL_Rect stack[STACK_MAX_SIZE]; // TODO make this configurable
   int stackSz = 0;
   for (auto it = items.rbegin(); it != items.rend(); it++) {
-    auto c = it->color;
-    if (c.a == 0) {
-      if (it->rect.w == 0) {
-        SDL_assert(stackSz > 0);
-        --stackSz;
-      } else {
-        SDL_assert(stackSz < STACK_MAX_SIZE);
-        stack[stackSz++] = it->rect;
-      }
+    if (it->type == POP_CLIP) {
+      SDL_assert(stackSz > 0);
+      --stackSz;
       SDL_RenderSetClipRect(renderer,
                             stackSz > 0 ? &stack[stackSz - 1] : nullptr);
-    } else if (it->texture != nullptr) {
-      SDL_SetTextureColorMod(it->texture, c.r, c.g, c.b);
-      if (it->srcRect.w) {
-        SDL_RenderCopy(renderer, it->texture, &it->srcRect, &it->rect);
-      } else {
-        SDL_RenderCopy(renderer, it->texture, nullptr, &it->rect);
-      }
-    } else {
+      continue;
+    }
+    if (it->type == PUSH_CLIP) {
+      SDL_assert(stackSz < STACK_MAX_SIZE);
+      stack[stackSz++] = it->rect;
+      SDL_RenderSetClipRect(renderer, &it->rect);
+      continue;
+    }
+    auto& shape = it->shape;
+    auto c = shape.color;
+    if (shape.texture == nullptr) {
       SDL_SetRenderDrawColor(renderer, c.r, c.g, c.b, c.a);
-      SDL_RenderFillRect(renderer, &it->rect);
+      SDL_RenderFillRect(renderer, &shape.rect);
+    }
+    SDL_SetTextureColorMod(shape.texture, c.r, c.g, c.b);
+    if (shape.srcRect.w) {
+      SDL_RenderCopy(renderer, shape.texture, &shape.srcRect, &shape.rect);
+    } else {
+      SDL_RenderCopy(renderer, shape.texture, nullptr, &shape.rect);
     }
   }
   SDL_SetRenderDrawBlendMode(renderer, blendMode);
