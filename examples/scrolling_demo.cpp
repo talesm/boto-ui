280a73bbc7c40ca33d5824a7fb9e83d544432717
talesm <tales.miranda84@gmail.com>
2021-01-05 11:37:12 -0300

    Add scrolling_demo

---
+++ examples/scrolling_demo.cpp
@@ -0,0 +1,63 @@
+#include <SDL.h>
+#include "dui.hpp"
+
+int
+main(int argc, char** argv)
+{
+  // Init SDL
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    fprintf(stderr, "%s\n", SDL_GetError());
+    return 1;
+  }
+
+  // Create window and renderer
+  SDL_Window* window = nullptr;
+  SDL_Renderer* renderer = nullptr;
+  if (SDL_CreateWindowAndRenderer(
+        800, 600, SDL_WINDOW_SHOWN, &window, &renderer) < 0) {
+    fprintf(stderr, "%s\n", SDL_GetError());
+    return 1;
+  }
+
+  // Create ui state
+  dui::State state{renderer};
+
+  // variables
+  int intVar = 10;
+
+  // Main loop
+  for (;;) {
+    // Event handling
+    SDL_Event ev;
+    while (SDL_PollEvent(&ev)) {
+      // Send event to the state
+      state.event(ev);
+
+      // Normal event handling
+      if (ev.type == SDL_QUIT) {
+        return 0;
+      }
+    }
+
+    // Begin Frame
+    auto f = dui::frame(state);
+
+    // UI
+    auto g = dui::group(f, "main", {0});
+    dui::numberField(g, "intVar1", &intVar);
+    dui::scrollBar(g, "intScroll1", &intVar, 0, 100);
+    g.end();
+
+    // Clear screen
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderFillRect(renderer, nullptr);
+
+    // End frame and render state
+    f.render();
+
+    // Present and wait a bit
+    SDL_RenderPresent(renderer);
+    SDL_Delay(1);
+  }
+  return 1;
+}
